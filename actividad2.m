f3e24a350137cc2b2b92392df288de1e80fb9827
Ignacio Ibañez <ignacio.ibanez@usach.cl>
2017-04-20 10:18:07 -0300

    solución actividad 2

---
+++ actividad2.m
@@ -0,0 +1,33 @@
+%actividad 2
+
+num3 = [0 0 4 10]; % 4s+10
+den3 = [4 9 5 0];  % 4s³+9s²+5s
+
+[s_num1, s_den1] = cloop(num3, den3);
+
+num4 = [0 0 0 1];  % 1
+den4 = [0 0 7 10]; %7s + 10
+
+num5 = [0 0 8 8];  % 8s + 8
+den5 = [1 2 3 0];  % s³+2s²+3s
+
+[s_num2, s_den2] = parallel(num4, den4, num5, den5);
+
+num6 = [0 0 3 2];  % 3s + 2
+den6 = [0 5 7 10]; % 5s² + 7s +10
+
+[s_num3, s_den3] = series(s_num1, s_den1, s_num2, s_den2);
+
+[s_num3, s_den3] = series(s_num3, s_den3, num6, den6);
+
+num1 = [0 0 4 0];  % 4s
+den1 = [0 10 0 4]; % 10s² + 4
+
+num2 = [0 0 0 3];  % 3
+den2 = [0 0 6 16]; % 6s + 16
+
+[s_num4, s_den4] = parallel(num1, den1, num2, den2);
+
+[s_num4, s_den4] = parallel(s_num4, s_den4, s_num3, s_den3);
+
+step(s_num4, s_den4, 100);
